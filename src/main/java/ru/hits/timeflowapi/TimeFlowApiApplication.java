9ef046da4c1aa804b93ed96f177821529d41ab16
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2023-02-16 15:04:40 +0700

    Инициализация проекта

---
+++ src/main/java/ru/hits/timeflowapi/TimeFlowApiApplication.java
@@ -0,0 +1,13 @@
+package ru.hits.timeflowapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeFlowApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeFlowApiApplication.class, args);
+    }
+
+}
