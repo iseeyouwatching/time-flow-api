e22be723d2ed1133aff1383b9542dd818d3b7aeb
maulina2 <101575070+maulina2@users.noreply.github.com>
2023-02-17 22:30:08 +0700

    Добавила сущности и перечисления

---
+++ src/main/java/ru/hits/timeflowapi/model/entity/EmployeePostEntity.java
@@ -0,0 +1,30 @@
+package ru.hits.timeflowapi.model.entity;
+
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "employee_post")
+public class EmployeePostEntity {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    private UUID id;
+
+    private String value;//teacher or scheduleMaker or admin
+
+    @ManyToMany(mappedBy = "posts")
+    private List<EmployeeDetailsEntity> employees;
+}
---
+++ src/main/java/ru/hits/timeflowapi/model/entity/StudentDetailsEntity.java
@@ -0,0 +1,32 @@
+package ru.hits.timeflowapi.model.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "student")
+
+public class StudentDetailsEntity {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    private UUID id;
+
+    @OneToOne()
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private UserEntity user;
+    private String studentNumber;
+
+
+    //private StudentGroupEntity group;
+
+}
