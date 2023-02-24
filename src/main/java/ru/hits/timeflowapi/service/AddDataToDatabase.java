a010f90b7b3d82bccecf0c71aeb1a690ee166426
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2023-02-24 17:10:02 +0700

    Добавил создание учетной записи для админка при запуске проекта.

--- src/main/java/ru/hits/timeflowapi/service/AddDataToDatabase.java
+++ src/main/java/ru/hits/timeflowapi/service/AddDataToDatabase.java
@@ -3,11 +3,22 @@ package ru.hits.timeflowapi.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import ru.hits.timeflowapi.model.entity.EmployeeDetailsEntity;
 import ru.hits.timeflowapi.model.entity.EmployeePostEntity;
 import ru.hits.timeflowapi.model.entity.StudentGroupEntity;
+import ru.hits.timeflowapi.model.entity.UserEntity;
+import ru.hits.timeflowapi.model.enumeration.AccountStatus;
+import ru.hits.timeflowapi.model.enumeration.Role;
+import ru.hits.timeflowapi.model.enumeration.Sex;
+import ru.hits.timeflowapi.repository.EmployeeDetailsRepository;
 import ru.hits.timeflowapi.repository.EmployeePostRepository;
 import ru.hits.timeflowapi.repository.StudentGroupRepository;
+import ru.hits.timeflowapi.repository.UserRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,11 +26,15 @@ public class AddDataToDatabase {
 
     private final StudentGroupRepository studentGroupRepository;
     private final EmployeePostRepository employeePostRepository;
+    private final UserRepository userRepository;
+    private final EmployeeDetailsRepository employeeDetailsRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @EventListener(ApplicationReadyEvent.class)
     public void addData() {
         addStudentGroups();
         addEmployeePosts();
+        addAdmin();
     }
 
     private void addStudentGroups() {
@@ -67,6 +82,38 @@ public class AddDataToDatabase {
                         .postRole("ROLE_TEACHER")
                         .build()
         );
+    }
+
+    private void addAdmin() {
+        Optional<EmployeePostEntity> roleAdmin = employeePostRepository.findByPostRole("ROLE_ADMIN");
+
+        if (roleAdmin.isEmpty()) {
+            return;
+        }
+
+        UserEntity user = UserEntity
+                .builder()
+                .email("email@gmail.com")
+                .role(Role.ROLE_EMPLOYEE)
+                .name("Иван")
+                .surname("Иванов")
+                .patronymic("Иванович")
+                .accountStatus(AccountStatus.ACTIVATE)
+                .password(passwordEncoder.encode("root"))
+                .sex(Sex.MALE)
+                .build();
+
+        user = userRepository.save(user);
+
+        employeeDetailsRepository.save(
+                EmployeeDetailsEntity
+                        .builder()
+                        .user(user)
+                        .posts(List.of(roleAdmin.get()))
+                        .contactNumber("000000")
+                        .build()
+        );
+
     }
 
 }
