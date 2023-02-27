492c3b6075b15d670f5b2c374096b42dfee98228
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2023-02-27 19:25:01 +0700

    Убрал EmailAlreadyUsedException, вместо него добавил использование ConflictException

--- src/main/java/ru/hits/timeflowapi/service/CheckEmailService.java
+++ src/main/java/ru/hits/timeflowapi/service/CheckEmailService.java
@@ -2,7 +2,7 @@ package ru.hits.timeflowapi.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import ru.hits.timeflowapi.exception.EmailAlreadyUsedException;
+import ru.hits.timeflowapi.exception.ConflictException;
 import ru.hits.timeflowapi.repository.UserRepository;
 
 @Service
@@ -16,11 +16,11 @@ public class CheckEmailService {
      * то выбросится исключение.
      *
      * @param email почта.
-     * @throws EmailAlreadyUsedException выбрасывается, если заданная почта уже используется.
+     * @throws ConflictException выбрасывается, если заданная почта уже используется.
      */
     public void checkEmail(String email) {
         if (userRepository.existsByEmail(email)) {
-            throw new EmailAlreadyUsedException("Пользователь с такой почтой уже существует");
+            throw new ConflictException("Пользователь с такой почтой уже существует");
         }
     }
 }
