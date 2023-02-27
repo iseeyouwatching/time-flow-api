d0f72566e973aa5aebfaea1495bec80734df4ffa
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2023-02-27 23:18:38 +0700

    Теперь составитель расписаний с неактивированным аккаунтом не будет иметь доступ к редактированию расписания.

--- src/main/java/ru/hits/timeflowapi/security/UserDetailsImpl.java
+++ src/main/java/ru/hits/timeflowapi/security/UserDetailsImpl.java
@@ -5,6 +5,8 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import ru.hits.timeflowapi.model.entity.UserEntity;
+import ru.hits.timeflowapi.model.enumeration.AccountStatus;
+import ru.hits.timeflowapi.model.enumeration.Role;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -19,11 +21,9 @@ public class UserDetailsImpl implements UserDetails {
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         List<SimpleGrantedAuthority> authorities = new ArrayList<>();
-        authorities.add(new SimpleGrantedAuthority(user.getRole().toString()));
 
-        for (String role : roles) {
-            authorities.add(new SimpleGrantedAuthority(role));
-        }
+        authorities.add(new SimpleGrantedAuthority(user.getRole().toString()));
+        addPostsIntoAuthorities(authorities);
 
         return authorities;
     }
@@ -56,6 +56,20 @@ public class UserDetailsImpl implements UserDetails {
     @Override
     public boolean isEnabled() {
         return true;
+    }
+
+    /**
+     * Добавляет роли сотрудника в {@code authorities} тогда, когда пользователь является сотрудником,
+     * а его аккаунт активирован.
+     *
+     * @param authorities полномочия.
+     */
+    private void addPostsIntoAuthorities(List<SimpleGrantedAuthority> authorities) {
+        if (user.getRole() == Role.ROLE_EMPLOYEE && user.getAccountStatus() == AccountStatus.ACTIVATE) {
+            for (String role : roles) {
+                authorities.add(new SimpleGrantedAuthority(role));
+            }
+        }
     }
 
 }
