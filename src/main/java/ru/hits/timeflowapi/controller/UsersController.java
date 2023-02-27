07cf37979f2081f33490c2e1c7a87e8866210522
maulina2 <101575070+maulina2@users.noreply.github.com>
2023-02-28 04:10:19 +0700

    Добавила пагинацию к методу

--- src/main/java/ru/hits/timeflowapi/controller/UsersController.java
+++ src/main/java/ru/hits/timeflowapi/controller/UsersController.java
@@ -1,13 +1,14 @@
 package ru.hits.timeflowapi.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.hits.timeflowapi.model.dto.user.UserDto;
 import ru.hits.timeflowapi.service.UsersService;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -17,7 +18,14 @@ public class UsersController {
     private final UsersService usersService;
 
     @GetMapping("/admin")
-    List<UserDto> getUsers() {
-        return usersService.getUsers();
+    Page<UserDto> getUsers(
+            @RequestParam(required = false, defaultValue = "0") int pageNumber,
+            @RequestParam(required = false, defaultValue = "10") int pageSize,
+            @RequestParam(required = false, defaultValue = "ASC") Sort.Direction sortDirection) {
+
+        return usersService.getUsers(
+                pageNumber,
+                pageSize,
+                sortDirection);
     }
 }
--- src/main/java/ru/hits/timeflowapi/service/UsersService.java
+++ src/main/java/ru/hits/timeflowapi/service/UsersService.java
@@ -1,36 +1,33 @@
 package ru.hits.timeflowapi.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import ru.hits.timeflowapi.mapper.UserMapper;
 import ru.hits.timeflowapi.model.dto.user.UserDto;
 import ru.hits.timeflowapi.model.entity.UserEntity;
 import ru.hits.timeflowapi.repository.UserRepository;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class UsersService {
     private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
-    public List<UserDto> getUsers() {
-        List<UserEntity> users = userRepository.findAll();
+    private static final String property = "name";
 
-        List<UserDto> userDtos = new ArrayList<>();
 
-        for (UserEntity user : users) {
-            userDtos.add(new UserDto(
-                    user.getId(),
-                    user.getEmail(),
-                    user.getRole(),
-                    user.getName(),
-                    user.getSurname(),
-                    user.getPatronymic(),
-                    user.getAccountStatus(),
-                    user.getSex()));
-        }
+    public Page<UserDto> getUsers(int pageNumber,
+                                  int pageSize,
+                                  Sort.Direction direction) {
+        Pageable pageable = PageRequest.of(pageNumber, pageSize, direction);
 
-        return userDtos;
+        Page<UserEntity> users = userRepository.findAll(pageable);
+
+        return users.map(userMapper::userToUserDto);
+
     }
 }
