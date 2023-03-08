f969040426d54c2ed763fe9bba4fa4c64ffa0798
Konstantin Samoylenko <samoylenko.konstantin.3197@gmail.com>
2023-03-08 20:45:05 +0700

    Добавил AccessTokenNotValidException, RefreshTokenNotValidException и их отлавливание в ExceptionHandlerController

---
+++ src/main/java/ru/hits/timeflowapi/exception/AccessTokenNotValidException.java
@@ -0,0 +1,18 @@
+package ru.hits.timeflowapi.exception;
+
+/**
+ * Исключение, которое следует бросать, когда какие-то проблемы с {@code access} токеном.
+ * Например если токен: отсутсвует, истёк, подделан.
+ */
+public class AccessTokenNotValidException extends RuntimeException {
+
+    /**
+     * Конструктор.
+     *
+     * @param message текст исключения.
+     */
+    public AccessTokenNotValidException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/main/java/ru/hits/timeflowapi/exception/RefreshTokenNotValidException.java
@@ -0,0 +1,13 @@
+package ru.hits.timeflowapi.exception;
+
+/**
+ * Исключение, которое следует бросать, когда какие-то проблемы с {@code refresh} токеном.
+ * Например если токен: отсутсвует, истёк, подделан.
+ */
+public class RefreshTokenNotValidException extends RuntimeException {
+
+    public RefreshTokenNotValidException(String message) {
+        super(message);
+    }
+
+}
