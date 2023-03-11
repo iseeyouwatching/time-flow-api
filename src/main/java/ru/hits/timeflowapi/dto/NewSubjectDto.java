133719a67951aa2bb81c0053dac59e24d628791a
ZubanovaAlina <alinazubanova5@gmail.com>
2023-03-11 17:11:27 +0700

    Поправила валидацию у новых дто

--- src/main/java/ru/hits/timeflowapi/dto/NewSubjectDto.java
+++ src/main/java/ru/hits/timeflowapi/dto/NewSubjectDto.java
@@ -4,9 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class NewSubjectDto {
+    @NotEmpty(message = "Название предмета не может быть пустым.")
     private String name;
 }
--- src/main/java/ru/hits/timeflowapi/dto/studentgroup/NewStudentGroupDto.java
+++ src/main/java/ru/hits/timeflowapi/dto/studentgroup/NewStudentGroupDto.java
@@ -5,11 +5,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class NewStudentGroupDto {
     @Schema(description = "Номер учебной группы", example = "972102")
+    @NotNull(message = "Номер учебной группы не может быть пустым.")
     private Integer number;
 
 }
